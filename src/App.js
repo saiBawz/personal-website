012ffcf8f3f9e862a1dcbc460f76ba28a46ca8a7
Sainabou B <bawosainabou@gmail.com>
2026-01-08 19:43:06 +0000

    Update App.js
    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css' 
+import React, { useState } from 'react'; // Add { useState } inside the curly braces
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
@@ -7,20 +8,41 @@ import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 
 function App() {
+const [isDarkMode, setIsDarkMode] = useState(true);
+  const [fontSize, setFontSize] = useState(18);
+
+  const toggleTheme = () => setIsDarkMode(!isDarkMode);
+  const increaseFont = () => setFontSize(prev => Math.min(prev + 2, 32));
+  const decreaseFont = () => setFontSize(prev => Math.max(prev - 2, 14));
+
   return (
+    <div
+      className={isDarkMode ? 'home-container' : 'home-container light-mode'}
+      style={{ '--font-size': `${fontSize}px` }}
+    >
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
+        <Route path="/" element={<Layout isDarkMode={isDarkMode} 
+           toggleTheme={toggleTheme} 
+              increaseFont={increaseFont} 
+              decreaseFont={decreaseFont}
+          /> }>
+         <Route index element={<Home 
+         isDarkMode={isDarkMode} 
+    fontSize={fontSize}
+    toggleTheme={toggleTheme} 
+    increaseFont={increaseFont} 
+    decreaseFont={decreaseFont}
+     />} />
           <Route path="home" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="projects" element={<Projects />} />
           <Route path="contact" element={<Contact />} />
           
-        </Route>
-        
+        </Route> 
       </Routes>
     </Router>
+    </div>
   );
 }
 
