28de402132275a937dcb04354e2a1d81621c10c4
Sainabou B <bawosainabou@gmail.com>
2026-01-08 19:42:23 +0000

    Update Layout.js
    updated layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,20 +1,31 @@
+import React from 'react';
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
+import Header from './Header';
 import Footer from "./Footer";
 
 
-const Layout = () => {
+const Layout = ({ isDarkMode, toggleTheme, increaseFont, decreaseFont }) => {
     return (
-        <div className="d-flex flex-column min-vh-100">
-            <Header />
-            <main className="flex-grow-1 my-4 my-lg-5">
-                <div className="container bg-white rounded shadow-sm p-3 p-md-4">
-                    <Outlet />
-                </div>
-            </main>
-            <Footer />
-        </div>
-    );
+        <div className="layout-wrapper">
+      <Header isDarkMode={isDarkMode} /> 
+
+      {/* This renders the current page (Home, About, etc.) */}
+      <main>
+        <Outlet />
+      </main>
+
+      {/* PERSISTENT MENU: This stays visible on all pages */}
+      <div className="floating-accessibility-menu">
+        <button onClick={toggleTheme} title="Toggle Theme">
+          {isDarkMode ? '🌞' : '🌙'}
+        </button>
+        <button onClick={increaseFont} title="Increase Font">A+</button>
+        <button onClick={decreaseFont} title="Decrease Font">A-</button>
+      </div>
+
+      <Footer /> 
+    </div>
+  );
 };
 
 export default Layout;
